869ccca711b44e3f33ce33c6b1e9e40a2521b134
Egor Siropov <stafffsss@mail.ru>
2020-08-15 14:47:18 +0300

    nornal fuction all model

--- returns_10_people.py
+++ returns_10_people.py
@@ -4,7 +4,10 @@ import json
 
 
 def return_love(pr_key, data):
-    people_list = []
+    #спиок для вывода и для добавления в BD
+    people_list_print = []
+    #список для сохранения в JSON
+    poeple_list_return = []
     item = 0
     for person in data:
         link = f'https://vk.com/id{person["id"]}'
@@ -15,33 +18,42 @@ def return_love(pr_key, data):
         if photo == None:
             continue
         else:
+            print_result = {'number': item, 'link': link, 'photo': photo, 'favorite': 0}
+            print(print_result)
+            # добавляеться primary key основного пользователя для дальнейшего добавления в DB
+            result_DB = {'id': pr_key, 'link': link, 'photo': photo, 'favorite': 0}
+            people_list_print.append(result_DB)
+            result_return_JSON = {'link': link, 'photo': photo, 'favorite': 0}
+            poeple_list_return.append(result_return_JSON)
+
+
             if item > 1 and (item % 10 == 0):
                 query = input('If next press "n". Add to favorites press number. End press "q" ')
-                add_database = people_list[item - 10:]
+                add_database = people_list_print[item - 10:]
+
 
                 if query == 'q':
-                    # добавление каждые 10 человек в DB
+                    # добавление каждые 10 человек в DB модуля db.py
                     add_people(add_database)
                     break
                 elif query != ('q' and 'n'):
                     #добавление в избранное
                     try:
                         favorite = int(query)
-                        people_list[favorite]['favorite'] = 1
+                        poeple_list_return[favorite]['favorite'] = 1
+                        people_list_print[favorite]['favorite'] = 1
                         add_people(add_database)
+                        question = input('Favorite saved. If End press "q" ')
+                        if question == 'q':
+                            break
                     except:
                         print('Error input')
                         add_people(add_database)
-            print_result = {'number': item, 'link':link, 'photo':photo, 'favorite':0}
-            #добавляеться primary key основного пользователя для дальнейшего добавления в DB
-            result = {'id': pr_key, 'link':link, 'photo':photo, 'favorite':0}
-            people_list.append(result)
-            print(print_result)
+                        continue
             item += 1
-
     with open('result_json\people_10.json', 'w',) as file:
-        json.dump(people_list[item - 10:], file, ensure_ascii=False, indent=4)
-    return people_list[item - 10:]
+        json.dump(poeple_list_return[item - 10:], file, ensure_ascii=False, indent=4)
+    return poeple_list_return[item - 10:]
 
 
 
